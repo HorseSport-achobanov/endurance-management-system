fc6f90ea49079fe60065d61a25006e1e6ebf8d6b
achobanov <alexchobanov@gmail.com>
2025-02-02 20:39:51 +0200

    Finalize NTS.Nexus folder structure
    https://github.com/Not-Endurance/not-timing-system/issues/349

--- src/Nexus/NTS.Nexus.Functions.Write/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.Hosting;
-
-FunctionsDebugger.Enable();
-
-var host = new HostBuilder().ConfigureFunctionsWorkerDefaults().Build();
-
-host.Run();
