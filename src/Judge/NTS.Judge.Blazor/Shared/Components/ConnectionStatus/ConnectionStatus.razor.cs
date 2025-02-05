d408b6c6bc100bfd0ccec37bb21a54ca1197aaf5
Stoyan Goranov <35375994+StoyanGoranov@users.noreply.github.com>
2025-02-05 20:50:44 +0200

    implement and use ConnectionStatus component
    https://github.com/Not-Endurance/not-timing-system/issues/336

---
+++ src/Judge/NTS.Judge.Blazor/Shared/Components/ConnectionStatus/ConnectionStatus.razor.cs
@@ -0,0 +1,50 @@
+﻿using MudBlazor;
+using Not.Application.RPC;
+using Not.Application.RPC.SignalR;
+
+
+namespace NTS.Judge.Blazor.Shared.Components.ConnectionStatus;
+
+public partial class ConnectionStatus
+{
+    RpcConnectionStatus _rpcConnectionStatus;
+    bool _isConnected;
+    Color SpinnerColor { get; set; } = Color.Error;
+
+    [Inject]
+    IRpcSocket RpcSocket { get; set; } = default!;
+
+    [Inject]
+    IConnectionsCounter ConnectionsCount { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        RpcSocket.Error += HandleRpcErrors;
+        RpcSocket.ServerConnectionChanged += HandleServerConnectionChanged;
+        RpcSocket.ServerConnectionInfo += HandleServerConnectionInfo;
+    }
+
+    void HandleRpcErrors(object? sender, RpcError rpcError)
+    {
+        _isConnected = _rpcConnectionStatus.Equals(RpcConnectionStatus.Connected);
+    }
+
+    void HandleServerConnectionChanged(object? sender, RpcConnectionStatus e )
+    {
+        _rpcConnectionStatus = e;
+        _isConnected = _rpcConnectionStatus.Equals(RpcConnectionStatus.Connected);
+        if (_rpcConnectionStatus == RpcConnectionStatus.Disconnected)
+        {
+            SpinnerColor = Color.Error;
+        } else { 
+            SpinnerColor = Color.Warning;
+        }
+        StateHasChanged();
+    }
+
+    void HandleServerConnectionInfo(object? sender, string e) 
+    {
+        _isConnected = true;
+        StateHasChanged();
+    }
+}
