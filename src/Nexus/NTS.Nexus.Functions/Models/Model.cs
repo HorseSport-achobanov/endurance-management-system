0d32279d272656fc2f4f4c2058acbe8bfa15a4b1
achobanov <alexchobanov@gmail.com>
2025-02-15 15:01:06 +0200

    Use string as GUID serialization fails with Mongo.Driver
    https://github.com/Not-Endurance/not-timing-system/issues/349

--- src/Nexus/NTS.Nexus.Functions/Models/Model.cs
+++ src/Nexus/NTS.Nexus.Functions/Models/Model.cs
@@ -2,7 +2,7 @@
 
 public record Model
 {
-    public Guid TenantId { get; set; }
+    public string TenantId { get; set; } = default!;
     public int Id { get; set; }
     public string Name { get; set; } = default!;
 }
